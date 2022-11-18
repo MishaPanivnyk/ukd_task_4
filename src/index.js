53ae96cbb08562ee2056c6cf316ca99ce1cd6dcb
Misha Panivnyk <panivnykm@gmail.com>
2022-11-18 15:24:08 +0200

    add parcel

---
+++ src/index.js
@@ -0,0 +1,20 @@
+let arr2 = [1, 2, 3, 4, 5];
+
+function binarySearch(array, num) {
+  let low = 0;
+  let high = array.length - 1;
+
+  while (low <= high) {
+    let mid = Math.floor((low + high) / 2);
+    if (array[mid] === num) {
+      return mid;
+    } else if (array[mid] < num) {
+      low = mid + 1;
+    } else {
+      high = mid - 1;
+    }
+  }
+  return -1;
+}
+
+console.log(binarySearch(arr2, 4));
