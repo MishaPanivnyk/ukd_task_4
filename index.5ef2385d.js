173b1b8bddc9519bb0690cff7b6a540bff8437fb
MishaPanivnyk <88387140+MishaPanivnyk@users.noreply.github.com>
2022-11-23 12:22:32 +0000

    Deploying to gh-pages from @ MishaPanivnyk/ukd_task_4@26c6c4ffd65b0cfaf3d423e62e582426a466cecb 🚀

--- index.5ef2385d.js
+++
@@ -1,2 +0,0 @@
-console.log(`Index: ${function(e,n){let o=0,t=e.length-1;for(;o<=t;){let l=Math.floor((o+t)/2);if(e[l]===n)return l;e[l]<n?o=l+1:t=l-1}return-1}([1,2,3,4,5],2)}`);
-//# sourceMappingURL=index.5ef2385d.js.map
--- index.7daa4ee2.js
+++
@@ -1,2 +0,0 @@
-console.log("Index: ".concat(function(n,o){for(var r=0,t=n.length-1;r<=t;){var e=Math.floor((r+t)/2);if(n[e]===o)return e;n[e]<o?r=e+1:t=e-1}return-1}([1,2,3,4,5],2)));
-//# sourceMappingURL=index.7daa4ee2.js.map
